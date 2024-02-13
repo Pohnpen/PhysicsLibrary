ba29200e8cc29255de87e916a6586d2424c599d8
Pohnpen Daennanart <pohnpen.a@kkumail.com>
2024-02-13 21:25:52 +0700

    SIDerivedUnit

--- lib/si_unit.py
+++ lib/si_unit.py
@@ -35,7 +35,7 @@ class Second(SIBaseUnit):
     quantity = "time"
     symbol = "s"
 
-class Kevin(SIBaseUnit):
+class Kelvin(SIBaseUnit):
     quantity = "temperature"
     symbol = "K"
 
@@ -48,12 +48,35 @@ class Mole(SIBaseUnit):
     symbol = "mol"
 
 
+class SIDerivedUnit(SIBaseUnit):
+    si_unit_expression = None
+
+    def __str__(self):
+        return f"{self.value} {self.symbol} ({self.si_unit_expression})"
+
+class Joule(SIDerivedUnit):
+    quantity = "energy, work, heat"
+    symbol = "J"
+    si_unit_expression = "kg*(m**2/s**2)"
+
+class Inch(SIDerivedUnit):
+    quantity = "length"
+    symbol = "in"
+    si_unit_expression = "m/39.3700787"
+
+class MeterPerSecond(SIDerivedUnit):
+    quantity = "velocity"
+    symbol = "m/s"
+    si_unit_expression = "m/s" # Meter / Second
+
+class JoulesPerKelvin(SIDerivedUnit):
+    quantity = "heat"
+    symbol = "J/K"
+    si_unit_expression = "J/K"
 
 
 if __name__ == "__main__":
-    kg = Kilogram(53.2)
-    sec = Second(10)
-    print(kg * sec)
-    print(kg / sec)  # kg.__truediv__(sec)
-    #print(kg + sec)
-    print(kg + kg)
+    joule = Joule(1.0)
+    print(joule) # 1.0 J (kg*(m**2/s**2))
+    inch = Inch(1.0)
+    print(inch) # 1.0 J (kg*(m**2/s**2))
